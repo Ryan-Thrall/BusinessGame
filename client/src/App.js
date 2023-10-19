216ef951d8fe2a38bf77960128b22cb2119ebe58
Ryan-Thrall <ryanthrall04@gmail.com>
2023-10-19 01:41:21 -0600

    Added Auth0 Frontend and tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
