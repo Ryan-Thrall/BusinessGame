3b1c6033e15526dfd27399c631238d87449f1c82
Ryan-Thrall <ryanthrall04@gmail.com>
2023-10-30 01:05:51 -0600

    Laying Out Game Page

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -4,7 +4,88 @@ function Game() {
   return (
     <div>
       <p>Production Cards: </p>
-      <div className="border-solid border-2 border-black">Hello</div>
+      <div className="grid grid-cols-12">
+
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+        <div className="border-solid border-2 border-black text-center mx-2 my-2 h-48">
+          <div className="border-solid border-b-2 p-2 border-black">
+            <b>Hello</b>
+          </div>
+          <p>Wazzzuuuupppp</p>
+        </div>
+
+      </div>
     </div>
   )
 }
